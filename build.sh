161ce104be2046b6b0185f7d602c68f405547ae7
Juliano Marcelino <14352515+ojmarcelino@users.noreply.github.com>
2022-01-19 19:55:11 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Build irpfedocker docker image.
-docker build --compress --label ojmarcelinoirpfedocker --tag ojmarcelino/irpfedocker .
+docker build --compress --label ojmarcelinoirpfe --tag ojmarcelino/irpf .
